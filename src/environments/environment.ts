266d2fbdc9166c63fe6fb4f370bb619a056d3e54
DESKTOP-STL844A\Raju Hasan <raju_hasan_1990@hotmail.com>
2018-02-23 12:26:26 +0600

    commit on master

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD68v7_D4xj26HFNilvakRAgZAXruFT51s',
+    authDomain: 'ahsan09-firebase-demo.firebaseapp.com',
+    databaseURL: 'https://ahsan09-firebase-demo.firebaseio.com',
+    projectId: 'ahsan09-firebase-demo',
+    storageBucket: 'ahsan09-firebase-demo.appspot.com',
+    messagingSenderId: '1057517529341'
+  }
 };
